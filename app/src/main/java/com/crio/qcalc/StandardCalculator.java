74463bc417c36b82ce72865b59a81e50c4a6a09a
CrioUser <crio-user@criodo.com>
2023-09-07 16:57:11 +0530

    ME_QCALC_V3_MODULE_IMPLEMENTATION

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -6,6 +6,66 @@ public class StandardCalculator {
         System.out.println("Standard Calculator 1.0");
     }
 
+   private int result;
+public void add(int num1, int num2)
+{
+    
+    int result = 0;
+    result =  num1+num2;
+    setResult(result);
+}
+
+public void subtract(int num1, int num2){
+    
+    int result = 0;
+    result =  num1-num2;
+    setResult(result);
+}
+
+
+public void multiply(int num1, int num2)
+{
+    int result = 0;
+    result =  num1 * num2;
+    setResult(result);
+}
+
+public void divide(int num1, int num2)
+{
+     int result = 0;
+     result =  num1/num2;
+     setResult(result);
+
+}
+
+
+
+public int getResult() 
+{
+   return result;
+}
+
+
+private void setResult(int value){
+  this.result = value;
+}
+
+
+public void clearResult() {
+
+    result = 0;
+
+}
+
+
+public void printResult(){
+
+    System.out.println("Standard Calculator Result:"+ getResult());
+
+}
+
+
+
 
 
 
